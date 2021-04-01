f8096105acdbcb6dbc86fefd12453ffa7a37c1d6
zhaohuakui <17362113082@163.com>
2021-04-01 17:22:49 +0800

    请求地址更改

--- request/config.js
+++ request/config.js
@@ -19,7 +19,7 @@ export default {
 		// target: 'http://172.16.9.58:8100', // 开发版接口
 		// target: 'https://tcwgwdev.tospur.com:18443', // 开发版本接 口
 		// target: 'https://ztwgwtest.tospurhouse.net', // 测试版接口  
-		  target: 'https://tcwgwstag.tospurhouse.net', // 预发布接口
-		// target: 'https://tcwgwprod.tospurhouse.net', // 正式环境接口
+		// target: 'https://tcwgwstag.tospurhouse.net', // 预发布接口
+		target: 'https://tcwgwprod.tospurhouse.net', // 正式环境接口
 	}
 }
