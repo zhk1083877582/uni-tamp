ef1c4101b166a9ad6c91bb16b8c82bc2ea8b6bd3
shanshan <18410717570@163.com>
2022-03-31 12:09:51 +0800

    building更新，添加授权

--- src/pagesHouse/house/house.vue
+++ src/pagesHouse/house/house.vue
@@ -7,8 +7,26 @@
 <script>
   export default {
     onLoad(opt) {
+      let buildingId = '',
+          userId = '',
+          articleId = ''
+      if (opt.scene) {
+        const scene = decodeURIComponent(opt.scene)
+        let obj = {}
+        scene.split('&').forEach((item) => {
+          const key = item.split('=')[0]
+          obj[key] = item.split('=')[1]
+        })
+        userId = obj.uId || ''
+        buildingId = obj.bId || ''
+        articleId = obj.a
+      } else {
+        userId = opt.userId || ''
+        buildingId = opt.buildingId || ''
+        articleId = opt.articleId || ''
+      }
       uni.redirectTo({
-        url: `/pages_com/building/detail?buildingId=${opt.buildingId}&userId=${opt.userId}`
+        url: `/pages_com/building/detail?buildingId=${buildingId}&userId=${userId}&articleId=${articleId}`
       })
     }
   }
